346209550188c2c002a1020307b1183b7ffa4df9
davidgeorgewilliams <david@davidgeorgewilliams.com>
2025-04-06 21:10:25 +0900

    test: remove unused result field from TestTrueTask
    Remove unused boolean field that wasn't referenced in any methods.
    This improves code clarity by eliminating potential confusion
    about the field's purpose.

--- src/test/java/com/cynthiasystems/agentflow/tasks/examples/TestTrueTask.java
+++ src/test/java/com/cynthiasystems/agentflow/tasks/examples/TestTrueTask.java
@@ -6,14 +6,11 @@ import lombok.AccessLevel;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import lombok.experimental.FieldDefaults;
-import lombok.experimental.NonFinal;
 
 @Accessors(fluent = true)
 @Setter
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class TestTrueTask extends Task {
-  @NonFinal boolean result;
-
   @Override
   protected boolean shouldProcess() {
     return true;
